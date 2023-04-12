ffc37187723272fac043fa9d378052b5ffd336ac
gatsby <greenteacore@outlook.com>
2023-04-12 16:55:29 +0800

    en

--- client/src/components/context/Partner.js
+++ client/src/components/context/Partner.js
@@ -3,11 +3,18 @@ import haut_logo from '../../assets/haut_logo.png'
 import zzuli_logo from '../../assets/zzuli_logo.jpg'
 import hati_logo from '../../assets/hati_logo.png'
 import pony_logo from '../../assets/pony_logo.jpg'
+import styles from './Partner.module.css'
 
 export default function Partner() {
     return (
-        <>
-        
-        </>
+        <div className={styles.partner}>
+            <h2>合作伙伴</h2>
+            <div className={styles.partner_container}>
+            <img src={haut_logo} alt='haut' className={styles.partner_logo} />
+            <img src={zzuli_logo} alt='haut' className={styles.partner_logo}/>
+            <img src={pony_logo} alt='haut' className={styles.partner_logo_long}     />
+            </div>
+           
+        </div>
     )
 }
--- client/src/components/pages/HomePage.js
+++ client/src/components/pages/HomePage.js
@@ -2,11 +2,12 @@ import React from 'react'
 import PostList from '../PostList'
 import CommonLayout from '../layouts/CommonLayout'
 import Screen from '../context/Screen'
-
+import Partner from '../context/Partner'
 export default function Home() {
   return (
     <CommonLayout>
         <Screen />
+        <Partner />
     </CommonLayout>
   )
 }
