24953070048d97c5f674c0dbc5409407c58899ee
cherrieszzz <greenteacore@outlook.com>
2023-04-18 19:35:07 +0800

    database init

--- server/index.js
+++ server/index.js
@@ -1,8 +1,15 @@
 const express = require('express')
+const mongoose = require('mongoose');
 require('dotenv').config();
 const app = express();
 
 const port = process.env.PORT;
+
+mongoose.connect(process.env.DB_URL).then(()=> {
+    console.log("Database are connected successful!");
+}).catch((error) => {
+    console.log(error);
+})
 
 app.get('/', (req, res) => {
     res.send({
