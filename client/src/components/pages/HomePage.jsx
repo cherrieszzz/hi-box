bd55b2029cfa401aa4e00717dd1c278d7cdb6e5a
cherrieszzz <greenteacore@outlook.com>
2023-05-10 20:48:50 +0800

    初始化daisy UI

--- client/src/components/pages/HomePage.jsx
+++ client/src/components/pages/HomePage.jsx
@@ -5,15 +5,6 @@ import zzuli_logo from '../../assets/zzuli_logo.jpg'
 import hati_logo from '../../assets/hati_logo.png'
 import pony_logo from '../../assets/pony_logo.jpg'
 
-
-function PartnerCard({ partner }) {
-    return (
-        <div className='w-full h-full py-3 flex justify-center items-center bg-gray-200 '>
-            <img src={partner.img} alt={partner.name} className='max-h-[5rem] max-w-[12rem]' />
-        </div>
-    )
-}
-
 function Screen() {
   return (
     <div className='flex flex-col items-center space-y-3'>
@@ -32,83 +23,92 @@ function Screen() {
 }
 
 function Partner() {
-    const partnerData = [
-        {
-            name: 'haut',
-            img: haut_logo,
-        },
-        {
-            name: 'zzuli',
-            img: zzuli_logo,
-        },
-        {
-            name: 'pony',
-            img: pony_logo,
-        },
-        {
-            name: 'hati',
-            img: hati_logo
-        }
-    ]
 
+  function PartnerCard({ partner }) {
     return (
-        <div className='container w-sm mx-auto my-[15rem]'>
-            <div className='max-w-[50rem] mx-auto'>
-                <h2 className='font-black text-lg'>合作伙伴</h2>
-                <div className='grid grid-cols-3 gap-1 grid-flow-row max-w-[50rem] mx-auto'>
-                    {
-                        partnerData.map((partner) => {
-                            return (
-                                <PartnerCard partner={partner} />
-                            )
-                        })
-                    }
-                </div>
-            </div>
-
-        </div>
+      <div className='w-full h-full py-3 flex justify-center items-center bg-gray-200 '>
+        <img src={partner.img} alt={partner.name} className='max-h-[5rem] max-w-[12rem]' />
+      </div>
     )
+  }
+
+  const partnerData = [
+    {
+      name: 'haut',
+      img: haut_logo,
+    },
+    {
+      name: 'zzuli',
+      img: zzuli_logo,
+    },
+    {
+      name: 'pony',
+      img: pony_logo,
+    },
+    {
+      name: 'hati',
+      img: hati_logo
+    }
+  ]
+
+  return (
+    <div className='container w-sm mx-auto my-[15rem]'>
+      <div className='max-w-[50rem] mx-auto'>
+        <h2 className='font-black text-lg'>合作伙伴</h2>
+        <div className='grid grid-cols-3 gap-1 grid-flow-row max-w-[50rem] mx-auto'>
+          {
+            partnerData.map((partner) => {
+              return (
+                <PartnerCard partner={partner} />
+              )
+            })
+          }
+        </div>
+      </div>
+
+    </div>
+  )
 }
 
 
 function SectionPlat() {
   return (
-      <div className='flex max-w-[80rem] mx-auto my-[10rem] space-x-5 justify-around flex-wrap'>
-          <div className='max-w-[20rem] flex flex-col items-center space-y-[2rem]'>
-              <svg aria-hidden="true" className='w-[18rem]'> <use xlinkHref="#icon-shequhuodong"></use> </svg>
-              <h2 className='font-black text-2xl'>社区交流</h2>
-              <p className='text-gray-600'>
-                  欢迎加入我们的社群！无论你是想找到新的灵感，还是想结交更多的朋友，我们都为你提供了一个舒适的环境。快来加入我们，开启一段全新的旅程吧！
-              </p>
-          </div>
-          <div className='max-w-[20rem] flex flex-col items-center space-y-[2rem]'>
-              <svg aria-hidden="true" className='w-[18rem]'> <use xlinkHref="#icon-dingzhi"></use> </svg>
-              <h2 className='font-black text-2xl'>
-                  私人定制
-              </h2>
-              <p className='text-gray-600'>
-                  我们提供专业的私人订制服务，满足你的个性化需求。我们的团队将会与你紧密合作，确保你得到最满意的结果。快来尝试吧！
-              </p>
-          </div>
-          <div className='max-w-[20rem] flex flex-col items-center space-y-[2rem]'>
-              <svg aria-hidden="true" className='w-[18rem]'> <use xlinkHref="#icon-jiaoyu"></use> </svg>
-              <h2 className='font-black text-2xl'>
-                  科普专栏
-              </h2>
-              <p className='text-gray-600'>
-                  科普，从现在开始。我们的专栏涵盖了各种各样的科学知识，从生态环保到新技术，从基础知识到前沿研究。
-              </p>
-          </div>
+    <div className='flex max-w-[80rem] mx-auto my-[10rem] space-x-5 justify-around flex-wrap'>
+      <div className='max-w-[20rem] flex flex-col items-center space-y-[2rem]'>
+        <svg aria-hidden="true" className='w-[18rem]'> <use xlinkHref="#icon-shequhuodong"></use> </svg>
+        <h2 className='font-black text-2xl'>社区交流</h2>
+        <p className='text-gray-600'>
+          欢迎加入我们的社群！无论你是想找到新的灵感，还是想结交更多的朋友，我们都为你提供了一个舒适的环境。快来加入我们，开启一段全新的旅程吧！
+        </p>
       </div>
+      <div className='max-w-[20rem] flex flex-col items-center space-y-[2rem]'>
+        <svg aria-hidden="true" className='w-[18rem]'> <use xlinkHref="#icon-dingzhi"></use> </svg>
+        <h2 className='font-black text-2xl'>
+          私人定制
+        </h2>
+        <p className='text-gray-600'>
+          我们提供专业的私人订制服务，满足你的个性化需求。我们的团队将会与你紧密合作，确保你得到最满意的结果。快来尝试吧！
+        </p>
+      </div>
+      <div className='max-w-[20rem] flex flex-col items-center space-y-[2rem]'>
+        <svg aria-hidden="true" className='w-[18rem]'> <use xlinkHref="#icon-jiaoyu"></use> </svg>
+        <h2 className='font-black text-2xl'>
+          科普专栏
+        </h2>
+        <p className='text-gray-600'>
+          科普，从现在开始。我们的专栏涵盖了各种各样的科学知识，从生态环保到新技术，从基础知识到前沿研究。
+        </p>
+      </div>
+    </div>
   )
 }
 
 export default function Home() {
   return (
     <CommonLayout>
-        <Screen />
-        <SectionPlat />
-        <Partner />
+      <Screen />
+      <SectionPlat />
+      <Partner />
     </CommonLayout>
   )
 }
